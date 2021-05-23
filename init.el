9879c4ef4635e78b478073c7099af44dc5491315
commitHarakiri <53496273+alexandroviski@users.noreply.github.com>
2021-05-23 16:22:03 +0000

    Update init.el

--- init.el
+++ init.el
@@ -1,25 +1,13 @@
-;;;; ronnie's init.el |
-; 01 may 2021	      |	
-;=====================
+(trace-function
+ #'call-process nil
+ (lambda ()
+   (format "\nload-file-name: %s" load-file-name)))
 
-;; cleaning emacs interface
-(menu-bar-mode -1) ; disable menu
-(scroll-bar-mode -1) ; disable scrollbar
-(tool-bar-mode -1) ; disable toolbar
-(tooltip-mode -1) ; disable tooltips
-(set-fringe-mode 10) ; give some breathing room
-(setq visible-bell t) ; set up the visible bell
-
-;; tron legacy theme
-(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
-(load-theme 'tron-legacy t)
-;==========================
 
 (eval-and-compile
   (require 'package)
   (setq package-archives '(("elpa" . "https://elpa.gnu.org/packages/")
-                           ("marmalade" . "https://marmalade-repo.org/packages/")
-                           ("melpa" . "https://melpa.org/packages/")))
+			   ("melpa" . "https://melpa.org/packages/")))
   (package-initialize)
   ;; i always fetch the archive contents on startup and during compilation, which is slow
   (package-refresh-contents)
@@ -28,237 +16,100 @@
   (require 'use-package)
   ;; i don't really know why this isn't the default...
   (setf use-package-always-ensure t))
-
-;;; Text mode and Auto Fill mode
-; The next two lines put Emacs into Text mode
-; and Auto Fill mode, and are for writers who
-; want to start writing prose rather than code.
-(setq default-major-mode 'text-mode)
-(add-hook 'text-mode-hook 'turn-on-auto-fill)
-
-;;; Prevent Extraneous Tabs
-(setq-default indent-tabs-mode nil)
-;; turn on highlighting for search strings
-(setq search-highlight t)
-
-
-;; rust configuration
-(add-to-list 'load-path "~/.emacs.d/rust-emacs/")
-
-;;; Fonts
-
-(defvar monospace-font "Fira Mono"
-  "Preferred monospace font.")
-(defvar monospace-serif-font "Linux Libertine Mono"
-  "Preferred monospace serif font.")
-(defvar sans-serif-font "Fira Sans"
-  "Preferred sans serif font.")
-(defvar emoji-font "Noto Color Emoji"
-  "Preferred emoji font.")
-
-(defun font-installed-p (font)
-  "Check if FONT is avaliable in the system."
-  (member font (font-family-list)))
-
-(defun set-font-if-installed (face font)
-  "Set FONT as FACE's family if it is detected in the system."
-  (if (font-installed-p font)
-      (set-face-attribute face nil :family font)
-    (message "Font %s not installed!" font)))
-
-(defun set-fontset-font-if-installed (target font &optional add)
-  "Set FONT as a fontset font for TARGET if installed.
-See `set-fontset-font' for ADD."
-  (if (font-installed-p font)
-      (set-fontset-font t target font nil add)
-    (message "Font %s not installed!" font)))
-
-(defun do-set-font ()
-  "Actually set fonts.
-Will only run on the first creation of a graphic frame, otherwise
-font presence cannot be detected."
-  (when (display-multi-font-p)
-    (set-font-if-installed 'fixed-pitch-serif monospace-serif-font)
-    (set-font-if-installed 'variable-pitch sans-serif-font)
-    (set-font-if-installed 'default monospace-font)
-    (set-fontset-font-if-installed 'symbol emoji-font t)
-    (remove-hook 'server-after-make-frame-hook 'do-set-font)))
-
-(if (daemonp)
-    (add-hook 'server-after-make-frame-hook 'do-set-font)
-  (do-set-font))
-
-;; emacs dashboard
-(require 'use-package)
-(use-package dashboard
-  :ensure t
-  :config
-  (dashboard-setup-startup-hook))
-;; costumization
-(setq dashboard-banner-logo-title "Hacking is our weapon")
-(setq dashboard-startup-banner "~/.emacs.d/themes/banner.png")
-(setq dashboard-items '((recents  . 5)
-                        (bookmarks . 5)
-                        (projects . 5)
-                        (agenda . 5)
-                        (registers . 5)))
-(setq dashboard-set-navigator t)
-(setq dashboard-set-init-info t)
-
-; ================
-; teste do rust
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; rustic = basic rust-mode + additions
-
-(use-package rustic
-  :ensure
-  :bind (:map rustic-mode-map
-              ("M-j" . lsp-ui-imenu)
-              ("M-?" . lsp-find-references)
-              ("C-c C-c l" . flycheck-list-errors)
-              ("C-c C-c a" . lsp-execute-code-action)
-              ("C-c C-c r" . lsp-rename)
-              ("C-c C-c q" . lsp-workspace-restart)
-              ("C-c C-c Q" . lsp-workspace-shutdown)
-              ("C-c C-c s" . lsp-rust-analyzer-status)
-              ("C-c C-c e" . lsp-rust-analyzer-expand-macro)
-              ("C-c C-c d" . dap-hydra)
-              ("C-c C-c h" . lsp-ui-doc-glance))
-  :config
-  ;; uncomment for less flashiness
-  ;; (setq lsp-eldoc-hook nil)
-  ;; (setq lsp-enable-symbol-highlighting nil)
-  ;; (setq lsp-signature-auto-activate nil)
-
-  ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t)
-  (add-hook 'rustic-mode-hook 'rk/rustic-mode-hook))
-
-(defun rk/rustic-mode-hook ()
-  ;; so that run C-c C-c C-r works without having to confirm
-  (setq-local buffer-save-without-query t))
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; for rust-analyzer integration
-
-(use-package lsp-mode
-  :ensure
-  :commands lsp
-  :custom
-  ;; what to use when checking on-save. "check" is default, I prefer clippy
-  (lsp-rust-analyzer-cargo-watch-command "clippy")
-  (lsp-eldoc-render-all t)
-  (lsp-idle-delay 0.6)
-  (lsp-rust-analyzer-server-display-inlay-hints t)
-  :config
-  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-(use-package lsp-ui
-  :ensure
-  :commands lsp-ui-mode
-  :custom
-  (lsp-ui-peek-always-show t)
-  (lsp-ui-sideline-show-hover t)
-  (lsp-ui-doc-enable nil))
-
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; inline errors
-
-(use-package flycheck :ensure)
-
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; auto-completion and code snippets
-
-(use-package yasnippet
-  :ensure
-  :config
-  (yas-reload-all)
-  (add-hook 'prog-mode-hook 'yas-minor-mode)
-  (add-hook 'text-mode-hook 'yas-minor-mode))
-
-(use-package company
-  :ensure
-  :bind
-  (:map company-active-map
-              ("C-n". company-select-next)
-              ("C-p". company-select-previous)
-              ("M-<". company-select-first)
-              ("M->". company-select-last))
-  (:map company-mode-map
-        ("<tab>". tab-indent-or-complete)
-        ("TAB". tab-indent-or-complete)))
-
-(defun company-yasnippet-or-completion ()
-  (interactive)
-  (or (do-yas-expand)
-      (company-complete-common)))
-
-(defun check-expansion ()
-  (save-excursion
-    (if (looking-at "\\_>") t
-      (backward-char 1)
-      (if (looking-at "\\.") t
-        (backward-char 1)
-        (if (looking-at "::") t nil)))))
-
-(defun do-yas-expand ()
-  (let ((yas/fallback-behavior 'return-nil))
-    (yas/expand)))
-
-(defun tab-indent-or-complete ()
-  (interactive)
-  (if (minibufferp)
-      (minibuffer-complete)
-    (if (or (not yas/minor-mode)
-            (null (do-yas-expand)))
-        (if (check-expansion)
-            (company-complete-common)
-          (indent-for-tab-command)))))
-
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; for Cargo.toml and other config files
-
-(use-package toml-mode :ensure)
-
-
-;; -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-;; setting up debugging support with dap-mode
-(when (executable-find "lldb-mi")
-  (use-package dap-mode
-    :ensure
-    :config
-    (dap-ui-mode)
-    (dap-ui-controls-mode 1)
-
-    (require 'dap-lldb)
-    (require 'dap-gdb-lldb)
-    ;; installs .extension/vscode
-    (dap-gdb-lldb-setup)
-    (dap-register-debug-template
-     "Rust::LLDB Run Configuration"
-     (list :type "lldb"
-           :request "launch"
-           :name "LLDB::Run"
-	   :gdbpath "rust-lldb"
-           ;; uncomment if lldb-mi is not in PATH
-           ;; :lldbmipath "path/to/lldb-mi"
-           ))))
-
-(use-package rust-mode 
-	     :ensure t)
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(rust-mode yasnippet use-package toml-mode rustic lsp-ui flycheck exec-path-from-shell dashboard company)))
+ '(custom-safe-themes
+   '("cf9414f229f6df728eb2a5a9420d760673cca404fee9910551caf9c91cff3bfa" default))
+ '(package-selected-packages '(quelpa tron-legacy-theme use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+;; fonts
+;;(add-to-list 'bdf-directory-list "~/.emacs.d/bdf")
+
+;; paste image by tarhuntas
+(defun paste-clipboard-image-md5 (&optional dir)
+  (interactive)
+  (unless dir
+    (setq dir "pasted/"))
+  (unless (file-directory-p (file-name-directory dir))
+    (make-directory (file-name-directory dir))
+    (message (concat "Directory " (file-name-directory dir) " created.")))
+  (let (file-md5)
+    (setq file-md5
+	  (shell-command-to-string
+	    (concat
+	      "scrsht_file=$(mktemp);
+	      xclip -selection clipboard -t image/png -o > $scrsht_file;
+	      scrsht_md5=`md5sum $scrsht_file -b | head -c 32`;
+	      mv $scrsht_file "
+	      dir
+	      "$scrsht_md5.png;
+	      echo -n $scrsht_md5")))
+	      (insert (concat "[[file:pasted/" file-md5 ".png]]"))))
+
+(global-set-key (kbd "C-g") 'paste-clipboard-image-md5)
+;;===================================
+(load-theme 'tron-legacy)
+;; cleaning emacs interface
+(menu-bar-mode -1) ; disable menu
+(scroll-bar-mode -1) ; disable scrollbar
+(tool-bar-mode -1) ; disable toolbar
+(tooltip-mode -1) ; disable tooltips
+(set-fringe-mode 10) ; give some breathing room
+(setq visible-bell t) ; set up the visible bell
+
+;;------
+
+(unless (package-installed-p 'quelpa)
+  (with-temp-buffer
+    (url-insert-file-contents "https://raw.githubusercontent.com/quelpa/quelpa/master/quelpa.el")
+    (eval-buffer)
+    (quelpa-self-upgrade)))
+
+
+
+(quelpa '(telega :fetcher github
+               :repo "zevlg/telega.el"
+               :branch "master"
+               :files (:defaults "contrib" "etc" "server" "Makefile"))
+
+
+
+;; =====
+
+
+(create-fontset-from-fontset-spec
+  "-*-fixed-medium-r-normal-*-16-*-*-*-c-*-fontset-bdf,
+  japanese-jisx0208:-*-*-medium-r-normal-*-16-*-*-*-c-*-jisx0208.1983-*,
+  katakana-jisx0201:-*-*-medium-r-normal-*-16-*-*-*-c-*-jisx0201*-*,
+  latin-jisx0201:-*-*-medium-r-normal-*-16-*-*-*-c-*-jisx0201*-*,
+  japanese-jisx0208-1978:-*-*-medium-r-normal-*-16-*-*-*-c-*-jisx0208.1978-*,
+  thai-tis620:-misc-fixed-medium-r-normal--16-160-72-72-m-80-tis620.2529-1,
+  lao:-misc-fixed-medium-r-normal--16-160-72-72-m-80-MuleLao-1,
+  tibetan-1-column:-TibMdXA-fixed-medium-r-normal--16-160-72-72-m-80-MuleTibetan-1,
+  ethiopic:-Admas-Ethiomx16f-Medium-R-Normal--16-150-100-100-M-160-Ethiopic-Unicode,
+  tibetan:-TibMdXA-fixed-medium-r-normal--16-160-72-72-m-160-MuleTibetan-0")
+
+(setq font-encoding-alist
+      
+      (append '(("Portuguese-0"  (portuguese . 0))
+		("MuleTibetan-0" (tibetan . 0))
+		("GB2312"        (chinese-gb2312 . 0))
+		("JISX0208"      (japanese-jisx0208 . 0))
+		("JISX0212"      (japanese-jisx0212 . 0))
+		("VISCII"        (vietnamese-viscii-lower . 0))
+		("KSC5601"       (korean-ksc5601 . 0))
+		("MuleArabic-0"  (arabic-digit . 0))
+		("MuleArabic-1"  (arabic-1-column . 0))
+		("MuleArabic-2"  (arabic-2-column . 0)))
+	      font-encoding-alist))
+
+(set-frame-font "fontset-bdf")
+;;(set-face-attribute 'default t :font "Misc Fixed Regular" :height 100 )
